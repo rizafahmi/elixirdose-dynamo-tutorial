3cf365292f75509e97ebcb983700bb2992a719a3
Riza Fahmi <rizafahmi@gmail.com>
2014-03-31 22:29:53 +0700

    Using checkiz version of mongodb driver

--- mix.exs
+++ mix.exs
@@ -19,7 +19,9 @@ defmodule HelloDynamo.Mixfile do
   defp deps do
     [ { :cowboy, github: "extend/cowboy" },
       { :dynamo, "~> 0.1.0-dev", github: "elixir-lang/dynamo" },
-      { :mongoex, github: "mururu/mongoex", override: true }
+      { :bson, github: "checkiz/elixir-bson", tag: "0.1", override: true },
+      { :mongo, github: "checkiz/elixir-mongo", tag: "0.1" },
+      # { :finalizer, github: "meh/elixir-finalizer", tag: "v0.0.3", override: true }
     ]
   end
 end
